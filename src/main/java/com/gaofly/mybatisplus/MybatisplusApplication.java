7494c28c333151784d8d5b7bc7aa0ad29ca8afc8
gao <gao@126.com>
2022-09-17 22:11:56 +0800

    zz

--- src/main/java/com/gaofly/mybatisplus/MybatisplusApplication.java
+++ src/main/java/com/gaofly/mybatisplus/MybatisplusApplication.java
@@ -1,13 +1,16 @@
 package com.gaofly.mybatisplus;
 
+import com.gaofly.mybatisplus.service.UserserviceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MybatisplusApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MybatisplusApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MybatisplusApplication.class, args);
+        UserserviceImpl bean = run.getBean(UserserviceImpl.class);
     }
 
 }
